592a9915de6cdcb1ca7fe15379a7901e5a814ae4
Ekter Moi <ektermoi1@gmail.com>
2022-01-12 17:56:55 +0100

    ajout rapports sénce 4 + code bras+jambe

---
+++ Code/BAT_MONKEYYYY/BAT_MONKEYYYY.ino
@@ -0,0 +1,54 @@
+/*
+   MotorKnob
+
+   A stepper motor follows the turns of a potentiometer
+   (or other sensor) on analog input 0.
+
+   http://www.arduino.cc/en/Reference/Stepper
+   This example code is in the public domain.
+*/
+
+
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+Servo myservo2;
+Servo myservo3;  // create servo object to control a servo
+Servo myservo4;
+
+// create an instance of the stepper class, specifying
+// the number of steps of the motor and the pins it's
+// attached to
+
+// the previous reading from the analog input
+int previous = 0;
+
+void setup() {
+  // set the speed of the motor to 30 RPMs
+  myservo.attach(3);  // attaches the servo on pin 9 to the servo object
+  myservo2.attach(6);
+  myservo3.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo4.attach(10);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // get the sensor value
+  int val = analogRead(0);
+
+  // move a number of steps equal to the change in the
+  // sensor reading
+  //stepper.step(100);
+
+  // remember the previous value of the sensor
+  previous = val;
+
+  val = val / 5;   // scale it to use it with the servo (value between 0 and 180)
+  myservo3.write(val + 20);                // sets the servo position according to the scaled value
+  myservo4.write(180 - val * 1.5);
+  myservo.write(val + 64);                // sets the servo position according to the scaled value
+  myservo2.write(180 - val * 1.4);
+  delay(15);                           // waits for the servo to get there
+  Serial.println(val);
+}
--- Rapports/Rapports Nino Mulac/Rapports Nino Mulac.md
+++ Rapports/Rapports Nino Mulac/Rapports Nino Mulac.md
@@ -1,8 +1,11 @@
 Rapport du 10/11:
-    https://docs.google.com/document/d/1wCJqbtf6a1P_qU5FfE20af_YhquXBaHs7Ld5PVlToEI/edit?usp=sharing
+    [Lien](https://docs.google.com/document/d/1wCJqbtf6a1P_qU5FfE20af_YhquXBaHs7Ld5PVlToEI/edit?usp=sharing "Rapport du 10/11")
 
 Rapport du 15/12:
-    https://docs.google.com/document/d/1HZY7Ja-3lCBAXAP-2cRXQ6-ZGDIAnOfHKF5js3Zh_NY/edit?usp=sharing
+    [Lien](https://docs.google.com/document/d/1HZY7Ja-3lCBAXAP-2cRXQ6-ZGDIAnOfHKF5js3Zh_NY/edit?usp=sharing "Rapport du 15/12")
 
 Rapport du 05/01:
-    https://docs.google.com/document/d/1rSlfQrm6j42ahPPhHjHu3JUpascvxWhXL8LLcz4nMYo/edit?usp=sharing
+    [Lien](https://docs.google.com/document/d/1rSlfQrm6j42ahPPhHjHu3JUpascvxWhXL8LLcz4nMYo/edit?usp=sharing "Rapport du 05/01")
+
+Rapport du 12/01:
+    [Lien](https://docs.google.com/document/d/1z_liG3VIGFFYdneoDFyZR4zDu5JIN25a7xqjqtwNbV0/edit?usp=sharing "Rapport du 12/01")
